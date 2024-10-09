aa801c19a0ce6d3af3575ed82c703f5f3eefb6a8
fabrizia2 <melvinrenish56@gmail.com>
2024-10-10 02:15:16 +0300

    Renamed landing to index

--- statics/scripts/book.js
+++ statics/scripts/book.js
@@ -35,7 +35,7 @@ function renderServices(services) {
         serviceElement.classList.add('servicess-item');
         serviceElement.innerHTML = `
             <label class="servicess-item">
-                <input type="checkbox" class="servicess-checkbox" data-id="${service.id}" data-price="${service.price}" data-category-id="${service.categoryId}">
+                <input type="checkbox" class="servicess-checkbox" data-id="${service.id}" data-price="${service.price}" data-category-id="${service.category.id}">
                 <span>${service.name}</span>
                 <span class="price"> - Ksh. ${service.price}</span>
                 <div class="quantity-controls">
