12f17cd580fb9f877bcb035b51ca36700d827a82
fabrizia2 <melvinrenish56@gmail.com>
2024-10-31 00:17:45 +0300

    Addition of social media links

--- statics/scripts/footer.js
+++ statics/scripts/footer.js
@@ -5,9 +5,10 @@ class MyFooter extends HTMLElement {
         <footer>
             <p>Contact us at <a href="mailto:info@eventeasy.com">info@eventeasy.com</a></p>
             <ul>
-                <li><a href="https://facebook.com" target="_blank"><i class="fab fa-facebook"></i></a></li>
-                <li><a href="https://twitter.com" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                <li><a href="https://instagram.com" target="_blank"><i class="fab fa-instagram"></i></a></li>
+                <li><a href="https://www.facebook.com/profile.php?id=61565886325908&mibextid=ZbWKwL" target="_blank"><i class="fab fa-facebook"></i></a></li>
+                <li><a href="https://www.linkedin.com/company/eventeasy-kenya/" target="_blank"><i class="fab fa-linkedin"></i></a></li>
+                <li><a href="https://www.tiktok.com/@eventeasykenya?_t=8qc9C7RHOFx&_r=1" target="_blank"><i class="fab fa-tiktok"></i></a></li>
+                <li><a href="https://www.instagram.com/eventeasy_kenya/" target="_blank"><i class="fab fa-instagram"></i></a></li>
             </ul>
         </footer>
         `;
